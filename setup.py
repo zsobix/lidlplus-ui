4bf67e562d68e69c4206f704c917ad045995a7e9
zsobix <index@zsobix.xyz>
2026-04-12 13:12:54 +0200

    fix: didn't update the version code

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="lidlplus-ui",
-    version="0.0.2",
+    version="0.0.3",
     author="Zsombor Kalmar",
     description="Desktop version of the Lidl Plus mobile app using my own implementation of the Lidl Plus api.",
     long_description=long_description,
